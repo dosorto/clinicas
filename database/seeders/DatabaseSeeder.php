1581814a521e0165e3b7d951be76fd1878199fbb
DEV-SITR13 <dev-sitr13@iMac-de-sitr13-2.local>
2025-06-04 15:26:37 -0600

    Proyecto Laravel inicial

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'root',
+            'email' => 'root@example.com',
+        ]);
+        $this->call([
+            RolesAndPermissionsSeeder::class
+        ]);
+        $user = User::find(1);
+        $user->assignRole('root');
+
+    }
+}
