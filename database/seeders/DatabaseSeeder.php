64075e419a13db70acd34ee667db3a93bf39cd71
LuisFerAnd <luispuma1181@gmail.com>
2025-06-07 13:44:03 -0600

    v Luis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,5 +25,10 @@ class DatabaseSeeder extends Seeder
         $user = User::find(1);
         $user->assignRole('root');
 
+        #$this->call([
+        #    ConsultaSeeder::class,
+        #    RecetaSeeder::class,
+        #]);
+
     }
 }
