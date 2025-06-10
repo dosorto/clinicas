e149b28893152142edbdddf5617728a5784996e3
Fredy Castellon <cfredyedgardo@gmail.com>
2025-06-09 22:08:24 -0600

    Commit_Actualizacion

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             //     ->on('nacionalidades')
             //     ->onDelete('set null');
 
-            +$table->rememberToken();
+            $table->rememberToken();
             $table->timestamps();
         });
 
