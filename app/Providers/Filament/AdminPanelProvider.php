a830427432d3278fc188120af9c94dbd743ff7eb
Efrain Reyes <joseeaguirree6@gmail.com>
2025-08-04 08:58:28 -0600

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,6 +42,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
+                \App\Filament\Widgets\CalendarioCitasWidget::class,
                 \App\Filament\Widgets\RecetarioWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
