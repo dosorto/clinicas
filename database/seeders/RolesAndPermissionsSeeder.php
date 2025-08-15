c725641d873258d9e98570aacdf8c62f1c319e6a
Efrain Reyes <joseeaguirree6@gmail.com>
2025-08-15 02:40:38 -0600

    Chequeando Permisos

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -67,6 +67,7 @@ class RolesAndPermissionsSeeder extends Seeder
         Permission::firstOrCreate(['name' => 'crear detallenomina']);
         Permission::firstOrCreate(['name' => 'crear examenes']); // NUEVO EXÁMENES
         Permission::firstOrCreate(['name' => 'crear citas']); // NUEVO
+        Permission::firstOrCreate(['name' => 'crear cai_correlativos']);
         Permission::firstOrCreate(['name' => 'crear cai_autorizaciones']);
         Permission::firstOrCreate(['name' => 'crear cuentas_por_cobrars']);
         Permission::firstOrCreate(['name' => 'crear servicio']);
@@ -141,12 +142,12 @@ class RolesAndPermissionsSeeder extends Seeder
         $roleAdmin->givePermissionTo([
             // VER
             'ver personas', 'ver nacionalidad', 'ver usuario', 'ver pacientes', 'ver medicocentromedico', 'ver enfermedades', 'ver centromedico', 'ver especialidad', 'ver especialidadmedicos', 'ver medicos', 'ver enfermedades_pacientes', 'ver recetas', 'ver consultas', 'ver contratomedico', 'ver nomina', 'ver detallenomina', 'ver citas', 'ver examenes', 'ver cai_correlativos', 'ver cai_autorizaciones', 'ver cuentas_por_cobrars', 'ver servicio', 'ver Impuesto', 'ver factura', 'ver descuento', 'ver factura_detalles', 'ver pagos_facturas',
-            // CREAR
-            'crear personas', 'crear nacionalidad', 'crear usuario', 'crear pacientes', 'crear medicocentromedico', 'crear centromedico', 'crear enfermedades', 'crear especialidad', 'crear especialidadmedicos', 'crear medicos', 'crear enfermedades_pacientes', 'crear recetas', 'crear consultas', 'crear contratomedico', 'crear nomina', 'crear detallenomina', 'crear examenes', 'crear cai_correlativos', 'crear cai_autorizaciones', 'crear cuentas_por_cobrars', 'crear servicio', 'crear Impuesto', 'crear factura', 'crear descuento', 'crear factura_detalles', 'crear pagos_facturas',
+                        // CREAR
+            'crear personas', 'crear nacionalidad', 'crear usuario', 'crear pacientes', 'crear medicocentromedico', 'crear centromedico', 'crear enfermedades', 'crear especialidad', 'crear especialidadmedicos', 'crear medicos', 'crear enfermedades_pacientes', 'crear recetas', 'crear consultas', 'crear contratomedico', 'crear nomina', 'crear detallenomina', 'crear examenes', 'crear citas', 'crear cai_correlativos', 'crear cai_autorizaciones', 'crear cuentas_por_cobrars', 'crear servicio', 'crear Impuesto', 'crear factura', 'crear descuento', 'crear factura_detalles', 'crear pagos_facturas',
             // ACTUALIZAR
             'actualizar personas', 'actualizar nacionalidad', 'actualizar usuario', 'actualizar pacientes', 'actualizar medicocentromedico', 'actualizar centromedico', 'actualizar enfermedades', 'actualizar especialidad', 'actualizar especialidadmedicos', 'actualizar medicos', 'actualizar enfermedades_pacientes', 'actualizar recetas', 'actualizar consultas', 'actualizar contratomedico', 'actualizar nomina', 'actualizar detallenomina', 'actualizar citas', 'actualizar examenes', 'actualizar cai_correlativos', 'actualizar cai_autorizaciones', 'actualizar cuentas_por_cobrars', 'actualizar servicio', 'actualizar Impuesto', 'actualizar factura', 'actualizar descuento', 'actualizar factura_detalles', 'actualizar pagos_facturas',
             // BORRAR
-            'borrar personas', 'borrar nacionalidad', 'borrar usuario', 'borrar pacientes', 'borrar centromedico', 'borrar medicocentromedico', 'borrar enfermedades', 'borrar especialidad', 'borrar especialidadmedicos', 'borrar medicos', 'borrar enfermedades_pacientes', 'borrar recetas', 'borrar consultas', 'borrar medicocentromedico', 'borrar centromedico', 'borrar contratomedico', 'borrar nomina', 'borrar detallenomina', 'borrar citas', 'borrar examenes', 'borrar cai_correlativos', 'borrar cai_autorizaciones', 'borrar cuentas_por_cobrars', 'borrar servicio', 'borrar Impuesto', 'borrar factura', 'borrar descuento', 'borrar factura_detalles', 'borrar pagos_facturas'
+            'borrar personas', 'borrar nacionalidad', 'borrar usuario', 'borrar pacientes', 'borrar centromedico', 'borrar medicocentromedico', 'borrar enfermedades', 'borrar especialidad', 'borrar especialidadmedicos', 'borrar medicos', 'borrar enfermedades_pacientes', 'borrar recetas', 'borrar consultas', 'borrar contratomedico', 'borrar nomina', 'borrar detallenomina', 'borrar citas', 'borrar examenes', 'borrar cai_correlativos', 'borrar cai_autorizaciones', 'borrar cuentas_por_cobrars', 'borrar servicio', 'borrar Impuesto', 'borrar factura', 'borrar descuento', 'borrar factura_detalles', 'borrar pagos_facturas'
         ]);
 
         $roleAdminCentro = Role::firstOrCreate(['name' => 'administrador']);
