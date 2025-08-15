7aa9bedd51c3f328ef2e33ed69eb16c28344200f
LuisFerAnd <luispuma1181@gmail.com>
2025-08-15 04:50:07 -0600

    V 1.2.8 LuisF

--- database/seeders/TipoPagoSeeder.php
+++ database/seeders/TipoPagoSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\tipopago;
+use App\Models\TipoPago;
 use App\Models\Centros_Medico;
 use Carbon\Carbon;
 
